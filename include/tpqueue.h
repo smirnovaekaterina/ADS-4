c31c019604162179e4f3c66a2a4cab66bcac07ae
smirnovaekaterina <75855500+smirnovaekaterina@users.noreply.github.com>
2021-05-12 19:29:00 +0300

    Смирнова

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,12 +5,79 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+ private:
+    T* arr;
+    int size;
+    int begin, end;
+    int count;
+    int stepBack(int index) {
+        int res = --index;
+        if (res < 0)
+            res += size + 1;
+        return res;
+    }
+    int stepForward(int index) {
+        int res = ++index;
+        if (res > size)
+            res -= size + 1;
+        return res;
+    }
+
+ public:
+    TPQueue() :
+        size(100),
+        begin(0), end(0), count(0) {
+        arr = new T[size + 1];
+    }
+    ~TPQueue() {
+        delete[] arr;
+    }
+
+    void push(const T& item) {
+        assert(count < size);
+        if (isEmpty()) {
+            arr[end] = item;
+        } else {
+            int current = stepBack(end);
+            if (arr[current].prior >= item.prior) {
+                arr[end] = item;
+            } else {
+                do {
+                    arr[stepForward(current)] = arr[current];
+                    current = stepBack(current);
+                } while (arr[current].prior < item.prior &&
+                    stepForward(current) != begin);
+                arr[stepForward(current)] = item;
+            }
+        }
+        end = stepForward(end);
+        count++;
+    }
+
+    T pop() {
+        assert(count > 0);
+        T item = arr[begin];
+        count--;
+        begin = stepForward(begin);
+        return item;
+    }
+
+    T get() const {
+        assert(count > 0);
+        return arr[begin];
+    }
+
+    bool isEmpty() const {
+        return count == 0;
+    }
+
+    bool isFull() const {
+        return count == size;
+    }
 };
 
 struct SYM {
   char ch;
   int  prior;
 };
-
 #endif // INCLUDE_TPQUEUE_H_
